c4c5c3679fb3e2df6d64f750670674267b0a1193
vamsi58c <35930414+vamsi58c@users.noreply.github.com>
2023-04-15 17:22:35 +0530

    Update test_calc.py

--- sources/test_calc.py
+++ sources/test_calc.py
@@ -17,8 +17,8 @@ class TestCalc(unittest.TestCase):
         """
         Test that the addition of two floats returns the correct result
         """
-        result = calc.add2('10.5', 2)
-        self.assertEqual(result, 12.5)
+        result1 = calc.add2('10.5', 2)
+        self.assertEqual(result1, 12.5)
 
     def test_add_strings(self):
         """
